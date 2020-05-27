34609dfec6e774e79988d6e8b6404ffe6ce7e386
LuLuGP93 <LGPrieto93@knights.ucf.edu>
2020-05-27 15:45:54 -0400

    Hand Cusors
    set up changing cursors

---
+++ Mita2DGit/Assets/Scripts/CursorM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorM : MonoBehaviour
+{
+    public Texture2D cursorArrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Cursor.visible = false;
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+   
+}
