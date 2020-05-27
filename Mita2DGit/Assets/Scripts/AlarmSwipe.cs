d299a6f7b64e069d18fb607a1a94b7437e21b67d
LuLuGP93 <LGPrieto93@knights.ucf.edu>
2020-05-27 14:10:41 -0400

    Swipe Mechanic
    implemented prototype swipe mechanic

---
+++ Mita2DGit/Assets/Scripts/AlarmSwipe.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class AlarmSwipe : MonoBehaviour
+{
+
+    public GameObject Phone;
+    public float x1;
+    public float x2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            x1 = Input.mousePosition.x;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            x2 = Input.mousePosition.x;
+
+            if (x2 > x1)
+            {
+                SceneManager.LoadScene("Day1_P2");
+            }
+        }
+    }
+}
