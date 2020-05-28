bb0822ffd944c7e05294676a77ea70ec6cd4f654
LuLuGP93 <LGPrieto93@knights.ucf.edu>
2020-05-28 03:12:32 -0400

    Wake Me Up B4 U GOGO
    implemented the wake up function

---
+++ Mita2DGit/Assets/Scripts/WakeMe.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WakeMe : MonoBehaviour
+{
+    int Clicks = 0;
+    public GameObject Bubble1;
+    public GameObject Bubble2;
+
+    public void Sleepyhead()
+    {
+
+        //void OnMouseDown()
+       // {
+             Clicks++; 
+       // }
+
+        Debug.Log(Clicks);
+        if (Clicks == 1)
+        {
+            Bubble1.SetActive(true);
+        }
+
+        if (Clicks == 2)
+        {
+            Bubble1.SetActive(false);
+            Bubble2.SetActive(true);
+        }
+
+        if (Clicks == 3)
+        {
+            Bubble2.SetActive(false);
+            SceneManager.LoadScene("Day1_P3");
+        }
+    }
+
+    public void Start()
+    {
+
+    }
+
+  
+
+    public void Update()
+    {
+       
+    }
+    
+  
+}
