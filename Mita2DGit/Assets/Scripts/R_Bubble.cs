bdaac39abcae1846eb7776321b0f3f2e8fd5668b
LuLuGP93 <LGPrieto93@knights.ucf.edu>
2020-05-29 04:43:10 -0400

    Speech Game Prototype
    Implemented speech game prototype

---
+++ Mita2DGit/Assets/Scripts/R_Bubble.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class R_Bubble : MonoBehaviour
+{
+    [SerializeField]
+    private Transform sbubRIGHT;
+    private Vector2 initialPosition;
+    private Vector2 mousePosition;
+    private float deltaX, deltaY;
+    public static bool locked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!locked)
+        {
+            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!locked)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (Mathf.Abs(transform.position.x - sbubRIGHT.position.x) <=0.5f &&
+            Mathf.Abs(transform.position.y - sbubRIGHT.position.y) <= 0.5f)
+        {
+            transform.position = new Vector2(sbubRIGHT.position.x, sbubRIGHT.position.y);
+            locked = true;
+        }
+        else
+        {
+            transform.position = new Vector2(initialPosition.x, initialPosition.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
