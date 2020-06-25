1b52e1cd268b1bd17f0c1f5ceb646b1318190988
LuLuGP93 <LGPrieto93@knights.ucf.edu>
2020-06-25 12:48:22 -0400

    PillMaze Update
    completed basic logic of pill maze

---
+++ Mita2DGit/Assets/Scripts/NewMazeGoal.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMazeGoal : MonoBehaviour
+{
+    public int PillPoint;
+    [SerializeField] ParticleSystem DigestParticle = null;
+    [SerializeField] ParticleSystem DigestParticle2 = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PillPoint = 0;
+    }
+
+    
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Pill")
+        {
+            PillPoint += 1;
+            Debug.Log(PillPoint);
+            Debug.Log("youve entered the stomach");
+            DigestParticle.Play();
+            Destroy(col.gameObject);
+        }
+        if (col.gameObject.tag == "Pill1")
+        {
+            PillPoint += 1;
+            //Debug.Log(PillPoint);
+            Debug.Log("youve entered the stomach");
+            DigestParticle.Play();
+            Destroy(col.gameObject);
+        }
+    }
+
+    /*
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        PillPoint += 1;
+        Debug.Log(PillPoint);
+        Destroy(gameObject);
+    }
+    */
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PillPoint == 2)
+        {
+            // Debug.Log("you did it!");
+            SceneManager.LoadScene("MitaWakePOV");
+        }
+    }
+}
